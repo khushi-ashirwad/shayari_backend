a596662e5af2194fb673530d91c317324b6955a0
khushi sutariya <khushisutariya272@gmail.com>
2023-11-30 13:20:28 +0530

    solve today api

--- controller/quotes-controller.js
+++ controller/quotes-controller.js
@@ -9,7 +9,9 @@ export const manageQuotesShayari = async (request, response) => {
     await newItem.save();
     response.status(200).json(newItem);
   } catch (error) {
-    response.status(500).json({ message: `Error creating the Quotes:${error}` });
+    response
+      .status(500)
+      .json({ message: `Error creating the Quotes:${error}` });
   }
 };
 
@@ -23,7 +25,7 @@ export const getallcontant = async (request, response) => {
     response.status(200).json(filteredcontent);
   } catch (error) {
     console.log(error);
-    response.status(500).json({ message:`Error fetching Quotes:${error}` });
+    response.status(500).json({ message: `Error fetching Quotes:${error}` });
   }
 };
 export const getIdQuotes = async (request, response) => {
@@ -36,7 +38,9 @@ export const getIdQuotes = async (request, response) => {
     }
   } catch (error) {
     console.log(error);
-    response.status(500).json({ message: `Error fetching the quotes:${error}` });
+    response
+      .status(500)
+      .json({ message: `Error fetching the quotes:${error}` });
   }
 };
 
@@ -54,7 +58,9 @@ export const updateQuotesshayari = async (request, response) => {
     }
     response.status(200).json(update);
   } catch (error) {
-    response.status(500).json({ message: `Error updating the content:${error}` });
+    response
+      .status(500)
+      .json({ message: `Error updating the content:${error}` });
   }
 };
 
@@ -66,7 +72,7 @@ export const deleteQuotesshayari = async (request, response) => {
     }
     response.json(content);
   } catch (error) {
-    response.status(500).json({message:`Error delete the content:${error}` });
+    response.status(500).json({ message: `Error delete the content:${error}` });
   }
 };
 const random = async () => {
@@ -82,16 +88,20 @@ const random = async () => {
   return result;
 };
 
-schedule.scheduleJob('0 0 * * *', async (request,response) => {
+schedule.scheduleJob("0 0 * * *", async () => {
   try {
     const today = new Date();
-    // today.setMinutes(today.getMinutes() - 2)
-    today.setHours(0, 0, 0 ,0)
+    today.setHours(0, 0, 0, 0);
 
-    const existingQuote = await Quotesshyari.findOne({
+    // Check if a user has added quotes today
+    const userAddedQuotesToday = await Quotesshyari.find({
       createdAt: { $gte: today },
     });
-    if (!existingQuote) {
+
+    if (userAddedQuotesToday.length > 0) {
+      console.log("User added quotes today:");
+    } else {
+      // If no user-added quote today, add a random quote
       const result = await random();
       const addQuoteResponse = await axios.post(
         "http://localhost:8001/dailycontent",
@@ -101,22 +111,65 @@ schedule.scheduleJob('0 0 * * *', async (request,response) => {
         }
       );
       console.log("Daily quote added successfully:", addQuoteResponse.data);
-    } else {
-      console.log("Quote already added today:", existingQuote);
     }
   } catch (error) {
-    console.error("Error adding daily quote:", error);
+    console.error("Error processing scheduled job:", error);
   }
 });
 
+// schedule.scheduleJob("*/1 * * * *", async () => {
+//   try {
+//     const fiveMinutesAgo = new Date();
+//     fiveMinutesAgo.setMinutes(fiveMinutesAgo.getMinutes() - 1);
+
+//     // Check if a user has added quotes within the last 5 minutes
+//     const userAddedQuotesLastFiveMinutes = await Quotesshyari.find({
+//       createdAt: { $gte: fiveMinutesAgo },
+//     });
+
+//     if (userAddedQuotesLastFiveMinutes.length > 0) {
+//       console.log("User added quotes within the last 5 minutes:", userAddedQuotesLastFiveMinutes);
+//     } else {
+//       // If no user-added quote in the last 5 minutes, add a random quote
+//       const result = await random();
+//       const addQuoteResponse = await axios.post(
+//         "http://localhost:8001/dailycontent",
+//         {
+//           content: result.content,
+//           category: result.category,
+//         }
+//       );
+//       console.log("Daily quote added successfully:", addQuoteResponse.data);
+//     }
+//   } catch (error) {
+//     console.error("Error processing scheduled job:", error);
+//   }
+// });
 export const dailyContentadd = async (request, response) => {
   try {
-    const newItem = new Quotesshyari(request.body);
-    await newItem.save();
-    response.status(200).json({ message: "Daily quote added successfully " });
-    return request.body;
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    // Check if a user has added quotes today
+    const userAddedQuotesToday = await Quotesshyari.find({
+      createdAt: { $gte: today },
+    }).populate("category").exec();
+    // console.log("useraddquotestoday", userAddedQuotesToday.category.type==="quotes");
+    if (userAddedQuotesToday.length > 0) {
+      // If user has already added a quote today, display an error
+      response
+        .status(400)
+        .json({ message: "You can only add one quote per day" });
+    } else {
+      // If no user-added quote today, save the new quote
+      const newItem = new Quotesshyari(request.body);
+      await newItem.save();
+      response.status(200).json({ message: "Daily quote added successfully" });
+    }
   } catch (error) {
-    response.status(500).json({ message: `Quote already added today:${error}` });
+    response
+      .status(500)
+      .json({ message: `Quote already added today:${error}` });
   }
 };
 
@@ -125,17 +178,25 @@ export const dailyContentget = async (request, response) => {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
-    const existingQuote = await Quotesshyari.findOne({
+    // Check if a user has added quotes today
+    const userAddedQuotesToday = await Quotesshyari.find({
       createdAt: { $gte: today },
     });
-
-    if (existingQuote) {
-      console.log(existingQuote);
-      response.status(200).json(existingQuote);
+  
+    if (userAddedQuotesToday.length > 0) {
+      // If the user has added a quote today, retrieve the latest one
+      const latestQuote = userAddedQuotesToday[userAddedQuotesToday.length - 1];
+      console.log("Latest user-added quote today:", latestQuote);
+      response.status(200).json(latestQuote);
     } else {
-      response.status(404).json({ message: "No quote added today." });
+      // If no user-added quote today, fetch a random quote
+      const result = await random();
+      console.log("Random quote:", result);
+      response.status(200).json(result);
     }
   } catch (error) {
-    response.status(500).json({ error: "Error retrieving daily quote:", error });
+    response
+      .status(500)
+      .json({ error: "Error retrieving daily quote:", error });
   }
 };
